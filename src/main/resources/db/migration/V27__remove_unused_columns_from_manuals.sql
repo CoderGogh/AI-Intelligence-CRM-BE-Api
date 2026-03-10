9d24190249efc1afe3aae066364329069a6aa40d
Hyejin Jang <ouo980718@gmail.com>
2026-03-11 02:10:18 +0900

    chore: flyway fix

--- src/main/resources/db/migration/V27__remove_unused_columns_from_manuals.sql
+++ src/main/resources/db/migration/V27__remove_unused_columns_from_manuals.sql
@@ -13,23 +13,24 @@ ALTER TABLE manuals DROP COLUMN category;
 ALTER TABLE manuals DROP COLUMN created_by;
 
 -- 2. 새로운 연관 관계 컬럼 추가
-ALTER TABLE manuals 
-    ADD COLUMN category_code VARCHAR(20) NOT NULL,
-    ADD COLUMN emp_id INT NOT NULL; 
+ALTER TABLE manuals
+    ADD COLUMN category_code VARCHAR(20) NOT NULL,
+    ADD COLUMN emp_id INT NOT NULL;
 
 -- 3. 외래키(Foreign Key) 제약 조건 추가
+
 -- (1) 상담 카테고리 정책 테이블 연결
-ALTER TABLE manuals 
-    ADD CONSTRAINT fk_manuals_category_policy
-    FOREIGN KEY (category_code) 
-    REFERENCES consultation_category_policy (category_code);
+ALTER TABLE manuals
+    ADD CONSTRAINT fk_manuals_category_policy
+    FOREIGN KEY (category_code)
+    REFERENCES consultation_category_policy (category_code);
 
 -- (2) 직원 테이블 연결
-ALTER TABLE manuals 
-    ADD CONSTRAINT fk_manuals_employee
-    FOREIGN KEY (emp_id) 
-    REFERENCES employees (emp_id);
+ALTER TABLE manuals
+    ADD CONSTRAINT fk_manuals_employee
+    FOREIGN KEY (emp_id)
+    REFERENCES employees (emp_id);
 
 -- 4. 조회 성능 향상을 위한 인덱스 추가
 CREATE INDEX idx_manuals_category_code ON manuals (category_code);
-CREATE INDEX idx_manuals_emp_id ON manuals (emp_id); 
+CREATE INDEX idx_manuals_emp_id ON manuals (emp_id);
