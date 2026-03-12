e47fa740d3729017a94ecdd7ac17494f7bcd006b
hyuck <nish11071@naver.com>
2026-03-12 17:41:18 +0900

    fix/flyway v33 수정

--- src/main/resources/db/migration/V33__Drop_unused_tables.sql
+++ src/main/resources/db/migration/V33__Drop_unused_tables.sql
@@ -1,5 +1,8 @@
--- 1. 기존에 사용하던 상담 분석 상태 테이블 삭제
-DROP TABLE IF EXISTS consultation_analysis_status;
+-- 1. 자식 테이블(user_bookmarks)에 걸려 있는 외래 키 제약 조건 먼저 삭제
+ALTER TABLE user_bookmarks DROP FOREIGN KEY fk_bm_best_practice;
 
--- 2. 기존에 사용하던 우수 사례(명예의 전당) 테이블 삭제
+-- 2. 이제 부모 테이블 삭제 가능
 DROP TABLE IF EXISTS best_practices;
+
+-- 3. 기존에 사용하던 상담 분석 상태 테이블 삭제
+DROP TABLE IF EXISTS consultation_analysis_status;
